d54b9d398b1b995009d08007172d39bddf18ec9d
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-16 23:47:02 +0000

    Exported file

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
-const net = require("net");
+const client = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Hi there!")
-  });
-  conn.on("data", (messageFromServer) => {
-    console.log(messageFromServer);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
-connect();
+client.connect();
