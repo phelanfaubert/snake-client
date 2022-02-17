c1c377280daf1803cd5be0ef47bc0425520e03cd
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-17 03:31:21 +0000

    Implemented stdin

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
-const client = require("./client");
+//const client = require("./client");
+const {connect} = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }}
+
+setupInput()
 
 console.log("Connecting ...");
-client.connect();
+//client.connect();
+connect();
+
+
+
