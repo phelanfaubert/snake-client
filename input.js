7590af4692bc347f119a4c295f0aa6e7a81161df
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-18 22:01:37 +0000

    Allowed movement with keys

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,8 +11,21 @@ const setupInput = function () {
 };
 
 const handleUserInput = function (key) {
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+  if (key ===  's') {
+    connection.write("Move: down")
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
   if (key === '\u0003') {
     process.exit();
-  }}
+  }
+};
 
-  module.exports = {setupInput};
+module.exports = { setupInput };
