bbe127cb10d647d02217355130f2754b4b48bf4f
Phelan Faubert <phelanfaubert@gmail.com>
2022-02-18 22:02:36 +0000

    Refactored

--- play.js
+++ play.js
@@ -1,26 +1,11 @@
-//const client = require("./client");
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }}
-
-setupInput()
 
 console.log("Connecting ...");
-//client.connect();
-connect();
+
+setupInput((connect()));
+
 
 
 
